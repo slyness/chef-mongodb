eb2f569c9abfa2aba592cb4b64781b9c8cba4077
Aaron Baer <aaron@hw-ops.com>
2015-05-08 09:02:34 -0700

    remove file ownsership as this should be in wrapper

--- recipes/replicaset.rb
+++ recipes/replicaset.rb
@@ -22,46 +22,6 @@ node.set['mongodb']['cluster_name'] = node['mongodb']['cluster_name']
 
 include_recipe 'mongodb::install'
 include_recipe 'mongodb::mongo_gem'
-require 'etc'
-
-if ::File.exist?("/data/admin.0")
-  if ::File.stat("/data/admin.0").uid == 220
-    data_uid = 0
-  else
-    data_uid = ::File.stat("/data/admin.0").uid
-  end
-
-  execute "Make sure that data directory has correct permissions" do
-    command "chown -R #{node['mongodb']['user']} /data"
-    not_if { ::Etc.getpwuid(data_uid).name == node['mongodb']['user'] }
-  end
-end
-
-if ::File.exist?("/log/mongodb/mongodb.log")
-  if ::File.stat("/log/mongodb/mongodb.log").uid == 220
-    log_uid = 0
-  else
-    log_uid = ::File.stat("/log/mongodb/mongodb.log").uid
-  end
-
-  execute "Make sure that log directory has correct permissions" do
-    command "chown -R #{node['mongodb']['user']} /log"
-    not_if { ::Etc.getpwuid(log_uid).name == node['mongodb']['user'] }
-  end
-end
-
-if ::File.exist?("/journal/prealloc.0")
-  if ::File.stat("/journal/prealloc.0").uid == 220
-    journal_uid = 0
-  else
-    journal_uid = ::File.stat("/journal/prealloc.0").uid
-  end
-
-  execute "Make sure that journal directory has correct permissions" do
-    command "chown -R #{node['mongodb']['user']} /journal"
-    not_if { ::Etc.getpwuid(journal_uid).name == node['mongodb']['user'] }
-  end
-end
 
 unless node['mongodb']['is_shard']
   mongodb_instance node['mongodb']['instance_name'] do
